ea91d344930c0f127fc4345e7200b670295ba08f
Phil Arfuso <arfusop@gmail.com>
2023-04-02 21:56:24 -0400

    initial dependencies and setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import logo from '@/assets/gg logo.jpg'
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Going Gold Choreography</title>
+                <meta
+                    name="description"
+                    content="Info about Going Gold Choreography"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href={logo.src} />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
